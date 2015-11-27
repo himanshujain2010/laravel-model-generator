415ab80d8da82a692afae7a1e5d8d9e5335dac95
Himanshu Jain <himanshujain.2792@gmail.com>
2015-11-27 14:14:11 +0530

    CloudSite removed from model name

--- src/Utilities/VariableConversion.php
+++ src/Utilities/VariableConversion.php
@@ -17,7 +17,7 @@ class VariableConversion
     public static function convertTableNameToClassName($table)
     {
         $string = str_replace(' ', '', ucwords(str_replace('_', ' ', $table)));
-
+        $string = str_replace('CloudSite','',$string);
         return $string;
     }
 
